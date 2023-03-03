c06a9d204827ff680aea53a169905f27e035c692
furkantozkoparan <125921593+furkantozkoparan@users.noreply.github.com>
2023-03-03 16:03:47 +0300

    Update Task_3.sql

--- Task_3.sql
+++ Task_3.sql
@@ -16,3 +16,18 @@ Select * from customer
 where store_id = '1'
 order by last_name desc
 limit 4;
+
+--Film tablosunda bulunan rental_rate sütunundaki değerlerin ortalaması nedir?
+Select avg(rental_rate) from film;
+
+--Film tablosunda bulunan filmlerden kaç tanesi 'C' karakteri ile başlar?
+select count (*) from film
+where title like 'C%';
+
+--Film tablosunda bulunan filmlerden rental_rate değeri 0.99 a eşit olan en uzun (length) film kaç dakikadır?
+Select max(length) from film
+where rental_rate = 0.99;
+
+--Film tablosunda bulunan filmlerin uzunluğu 150 dakikadan büyük olanlarına ait kaç farklı replacement_cost değeri vardır?
+Select count(distinct replacement_cost) from film
+where length > 150;
