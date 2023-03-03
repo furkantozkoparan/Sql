93c486ae9a6fe3bd1f6d3b4f264944021bd21680
furkantozkoparan <125921593+furkantozkoparan@users.noreply.github.com>
2023-03-03 16:50:20 +0300

    Update Task_4.sql

--- Task_4.sql
+++ Task_4.sql
@@ -1,1 +1,19 @@
+--Film tablosunda bulunan filmleri rating değerlerine göre gruplayınız.
+Select rating, count(*) from film
+group by rating;
+
+--Film tablosunda bulunan filmleri replacement_cost sütununa göre grupladığımızda film sayısı 50 den fazla olan replacement_cost değerini ve karşılık gelen film sayısını sıralayınız.
+Select replacement_cost, count(*) from film
+group by replacement_cost
+having count(*) > 50;
+
+--Customer tablosunda bulunan store_id değerlerine karşılık gelen müşteri sayılarını nelerdir?
+Select store_id, count (*) from customer
+group by store_id;
+
+--City tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra en fazla şehir sayısı barındıran country_id bilgisini ve şehir sayısını paylaşınız.
+Select country_id, count(*) from city
+group by country_id
+order by count desc
+limit 1;
 
